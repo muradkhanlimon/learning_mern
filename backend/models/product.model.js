1e1c2f1c0c24eb2040d8dde856915a495b705ef7
muradkhanlimon <74227132+muradkhanlimon@users.noreply.github.com>
2025-03-15 00:29:09 +0600

    remove app.get() and add app.post() for product

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -21,5 +21,6 @@ const productSchema = new mongoose.Schema({
 });
 
 const Product = mongoose.model('Product', productSchema);
+//products
 
 export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready - 123.");
+app.post("/products", async (req, res) => {
+    const product = req.body; //user will send this data
+
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({success:false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true, data: newProduct});
+    } catch (error) {
+        conslole.error("Error in create product: ", error.message);
+        res.status(500).json({success:false, message: "Server Error"});
+    }
 });
 
 console.log(process.env.MONGO_URI);
