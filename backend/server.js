1e3f4a1df6cab1d159359d589a0aecaa3056e222
muradkhanlimon <74227132+muradkhanlimon@users.noreply.github.com>
2025-03-15 02:03:34 +0600

    add product.route.js and modify server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
+// import Product from './models/product.model.js';
+// import mongoose from 'mongoose';
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -10,62 +11,7 @@ const app = express();
 
 app.use(express.json()); //allows us to accept JSON data in the body
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({success: true, data: products});
-    } catch (error) {
-        console.log("error in fetching products: ", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.post("/api/products", async (req, res) => {
-    const product = req.body; //user will send this data
-
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({success:false, message: "Please provide all fields"});
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    } catch (error) {
-        conslole.error("Error in create product: ", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.put("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-
-    const product = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({success: false, message: "Invalid product id"});
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-        res.status(200).json({success: true, data: updatedProduct });
-    } catch (error) {
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product Deleted"});
-    } catch (error) {
-        onsole.log("error in deleting products: ", error.message);
-        res.status(404).json({success: false, message: "Product not found"});
-    }
-});
+app.use("/apt/products", productRoutes);
 
 console.log(process.env.MONGO_URI);
 
