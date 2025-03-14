5cc51ebf5064bd367f4bff4958238e91591983c9
muradkhanlimon <74227132+muradkhanlimon@users.noreply.github.com>
2025-03-15 02:37:18 +0600

    tidy up backend | Hurray!

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -52,11 +52,15 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
     const { id } = req.params;
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({success: false, message: "Invalid product id"});
+    }
+    
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success: true, message: "Product Deleted"});
     } catch (error) {
         onsole.log("error in deleting products: ", error.message);
-        res.status(404).json({success: false, message: "Product not found"});
+        res.status(500).json({success: false, message: "Server Error"});
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(express.json()); //allows us to accept JSON data in the body
 
@@ -15,8 +16,8 @@ app.use("/api/products", productRoutes);
 
 console.log(process.env.MONGO_URI);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000")
+    console.log("Server started at http://localhost:" + PORT);
 });
 
