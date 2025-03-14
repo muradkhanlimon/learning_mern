99911cc4e107273adbbe3a1d1d86892c7b57373b
muradkhanlimon <74227132+muradkhanlimon@users.noreply.github.com>
2025-03-15 01:23:27 +0600

    modify  app.delete() in  server.js

--- backend/server.js
+++ backend/server.js
@@ -20,16 +20,22 @@ app.post("/api/products", async (req, res) => {
 
     try {
         await newProduct.save();
-        res.status(201).json({success:true, data: newProduct});
+        res.status(201).json({success: true, data: newProduct});
     } catch (error) {
         conslole.error("Error in create product: ", error.message);
-        res.status(500).json({success:false, message: "Server Error"});
+        res.status(500).json({success: false, message: "Server Error"});
     }
 });
 
 app.delete("/api/products/:id", async (req, res) => {
     const { id } = req.params;
-    console.log("id:", id);
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product Deleted"});
+    } catch (error) {
+        res.status(404).json({success: false, message: "Product not found"});
+    }
 });
 
 console.log(process.env.MONGO_URI);
